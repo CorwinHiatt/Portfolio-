2d8822725c64e408f3c91d25cb0c01c609918b82
Corwin Hiatt <corwinhiatt@yahoo.com>
2022-12-17 13:50:12 -0800

    just getting my green square 😅 -traveling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
+import {Route, Router, Routes} from 'react-router-dom';
+import Home from './pages/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      
+      
 
+    <Router>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,8 @@
 
 export default function Home() {
   return (
-    <div>
+    <div className="App">
       <h1>Home</h1>
-    </div>
+      </div>
   );
 }
